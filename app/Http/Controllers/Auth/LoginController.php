9b6877d47f1f1168840f1d399873419aced22422
Cyrizx <urbano.45@gmail.com>
2019-10-09 10:53:42 -0300

    cambio de ruta de logeo

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/ingresar';
 
     /**
      * Create a new controller instance.
